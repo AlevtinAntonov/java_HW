55254a8d71c321da7eac7558142121c7f7092392
Alevtin <antonov3017@gmail.com>
2022-12-08 09:33:42 +0300

    Added some corr Task_4.java

--- src/HW_1/Task_4.java
+++ src/HW_1/Task_4.java
@@ -5,29 +5,30 @@
 package HW_1;
 
 public class Task_4 {
+
     public static void main(String[] args) {
         String[] first = {"20", "?"};
         String[] second = {"?", "5"};
+        int e = 69;
         boolean f = false;
-        int res = 69;
 
         for (int i = 0; i < 10; i++) {
             int q = Integer.parseInt(first[0]) + i;
-
-            for (int j = 10; j < res - q; j += 10) {
+            for (int j = 10; j < e - q; j += 10) {
                 int w = Integer.parseInt(second[1]) + j;
-
-                if (q + w == 69) {
-                    System.out.printf("\nПервое слагаемое: %s , второе слагаемое: %s\n", q, w);
-                    System.out.printf("%s + %s = 69 ", q, w);
+                if (q + w == e) {
+                    System.out.printf("Решено. Первое слагаемое: %s , второе слагаемое: %s\n", q, w);
+                    System.out.printf("%s + %s = %s ", q, w, e);
                     f = true;
                     break;
                 }
+            }
+            if (f) {
+                break;
             }
         }
         if (!f) {
             System.out.print("Решения нет!");
         }
-
     }
 }
