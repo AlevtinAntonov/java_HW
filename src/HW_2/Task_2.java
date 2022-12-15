53b7630f3362d5d9e9458906321698229152367e
Alevtin <antonov3017@gmail.com>
2022-12-15 14:27:51 +0300

    Add logging after each iteration  Task_2.java

--- src/HW_2/Task_2.java
+++ src/HW_2/Task_2.java
@@ -1,4 +1,5 @@
-/*Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.*/
+/*Реализуйте алгоритм сортировки пузырьком числового массива,
+результат после каждой итерации запишите в лог-файл.*/
 package HW_2;
 
 import java.io.IOException;
@@ -10,27 +11,23 @@ import java.util.logging.SimpleFormatter;
 
 public class Task_2 {
     public static void main(String[] args) throws IOException {
+        int[] array = randomArray(10, 1, 100);
+        sortedBuble(array);
+    }
+
+    public static int[] sortedBuble(int[] array) {
         Logger logger = Logger.getLogger("Сортировка лог");
         FileHandler fh;
         logger.setUseParentHandlers(false);
-
-        int[] array = randomArray(10, 1, 100);
-        String log_1 = Arrays.toString(array);
-        int[] sorted_array = sortedBuble(array);
-        String log_2 = Arrays.toString(sorted_array);
-
         try {
             fh = new FileHandler("log.log", true);
             logger.addHandler(fh);
             fh.setFormatter(new SimpleFormatter());
-            logger.info("Исходный массив: " + log_1);
+            logger.info("Исходный массив: " + Arrays.toString(array));
         } catch (IOException exception) {
             exception.printStackTrace();
         }
-        logger.info("Отсортированный массив: " + log_2);
-    }
 
-    public static int[] sortedBuble(int[] array) {
         boolean isSorted = false;
         int tmp;
         while (!isSorted) {
@@ -43,9 +40,11 @@ public class Task_2 {
                     array[i] = array[i + 1];
                     array[i + 1] = tmp;
                 }
+                logger.info("Итерацция i=" + i + ", массив: " + Arrays.toString(array));
             }
+
         }
-        System.out.printf("\nОтсортированный массив: %s", Arrays.toString(array));
+        System.out.println("Отсортированный массив: " + Arrays.toString(array));
         return array;
     }
 
@@ -53,7 +52,7 @@ public class Task_2 {
         Random random;
         random = new Random();
         int[] array = random.ints(streamSize, randomNumberOrigin, randomNumberBound).toArray();
-        System.out.printf("Исходный массив: %s", Arrays.toString(array));
+        System.out.println("Исходный массив: " + Arrays.toString(array));
 
         return array;
     }
