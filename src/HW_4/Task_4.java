7cd2b8ba10c8388dfc873020d4aa06423f7416d8
Alevtin <antonov3017@gmail.com>
2022-12-19 16:21:31 +0300

    Add Task_4.java

---
+++ src/HW_4/Task_4.java
@@ -0,0 +1,8 @@
+/*** Реализовать алгоритм перевода из инфиксной записи в постфиксную для арифметического выражения. */
+package HW_4;
+
+public class Task_4 {
+    public static void main(String[] args) {
+
+    }
+}
