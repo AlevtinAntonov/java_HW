8e6b7b3879a8e23bc860db926529aa322a35a343
Alevtin <antonov3017@gmail.com>
2022-12-19 12:36:40 +0300

    Add MyLlist.java & Task_2.java

--- src/HW_4/MyLlist.java
+++ src/HW_4/MyLlist.java
@@ -4,26 +4,20 @@ import java.util.LinkedList;
 
 public class MyLlist {
     LinkedList<String> linkedList;
-    int[] array;
-    String item;
-    int size = 0;
-    int index = 0;
 
     public MyLlist() {
         linkedList = new LinkedList<>();
     }
 
     public void enqueue(String item) {
-        linkedList.add(item);
-        size++;
+        linkedList.offer(item);
     }
 
-    public int dequeue() {
-        size--;
-
+    public String dequeue() {
+        return linkedList.pollFirst();
     }
 
+    public String first() {
+        return linkedList.peekFirst();
+    }
 }
-/*enqueue() - помещает элемент в конец очереди,
-dequeue() - возвращает первый элемент из очереди и удаляет его,
-first() - возвращает первый элемент из очереди, не удаляя.*/
--- src/HW_4/Task_2.java
+++ src/HW_4/Task_2.java
@@ -5,5 +5,29 @@ first() - возвращает первый элемент из очереди, 
 (Подобную задачу решали на семинаре. Здесь так же нужно создать класс, который будет реализовывать указанные методы)*/
 package HW_4;
 
+import java.util.Arrays;
+
 public class Task_2 {
+    public static void main(String[] args) {
+        MyLlist myLlist = new MyLlist();
+
+        String str1 = "One";
+        String str2 = "Two";
+        String str3 = "Three";
+        String str4 = "Four";
+
+        myLlist.enqueue(str1);
+        myLlist.enqueue(str2);
+        myLlist.enqueue(str3);
+        myLlist.enqueue(str4);
+
+        String format = "%-25s %s\n";
+        System.out.format(format, Arrays.toString(myLlist.linkedList.toArray()), "- enqueue() - помещает элемент " +
+                "в конец очереди");
+        System.out.format(format, myLlist.dequeue(), "- dequeue() - возвращает первый элемент из очереди и удаляет его");
+        System.out.format(format, Arrays.toString(myLlist.linkedList.toArray()), "- массив после dequeue()");
+        System.out.format(format, myLlist.first(), "- first() - возвращает первый элемент из очереди, не удаляя");
+        System.out.format(format, Arrays.toString(myLlist.linkedList.toArray()), "- массив после first()");
+
+    }
 }
