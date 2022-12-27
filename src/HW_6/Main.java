538cb557817e8ccdd158bd0d4a80825677a96b6c
Alevtin <antonov3017@gmail.com>
2022-12-27 16:52:24 +0300

    Add HW_6

---
+++ src/HW_6/Main.java
@@ -0,0 +1,74 @@
+package HW_6;
+/*Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
+Создать множество ноутбуков.
+Написать метод, который будет запрашивать у пользователя критерий (или критерии) фильтрации и выведет ноутбуки,
+отвечающие фильтру. Критерии фильтрации можно хранить в Map. Например:
+“Введите цифру, соответствующую необходимому критерию:
+1 - ОЗУ
+2 - Объем ЖД
+3 - Операционная система
+4 - Цвет …
+Далее нужно запросить минимальные значения для указанных критериев - сохранить параметры фильтрации можно также в Map.
+Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.*/
+import com.sun.source.doctree.SeeTree;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+
+public class Main {
+    private static String noteBooksFile = "src/HW_6/notebooks.csv";
+
+    public static void main(String[] args) throws IOException {
+//        ArrayList<NoteBooks> noteBooks = loadNoteBooksFromFile();
+        NoteBooks noteBooks_1 = new NoteBooks(1000143954,"HP","620","1366x768","Intel Core 2 Duo T6670","2048Mb","320Gb",
+                48253,"Windows 10","silver");
+        NoteBooks noteBooks_2 = new NoteBooks(1000143955,"HP","620","1366x768","Intel Core 2 Duo T6670","2048Mb","320Gb",
+                48253,"Windows 10","silver");
+        NoteBooks noteBooks_3 = new NoteBooks(1000143956,"HP","620","1366x768","Intel Core 2 Duo T6670","2048Mb","320Gb",
+                48253,"Windows 10","silver");
+        NoteBooks noteBooks_4 = new NoteBooks(1000143957,"HP","620","1366x768","Intel Core 2 Duo T6670","2048Mb","320Gb",
+                48253,"Windows 10","silver");
+
+        Set<NoteBooks> noteBooksSet = new HashSet();
+        noteBooksSet.add(noteBooks_1);
+        noteBooksSet.add(noteBooks_2);
+        noteBooksSet.add(noteBooks_3);
+        noteBooksSet.add(noteBooks_4);
+
+        System.out.println(noteBooksSet);
+
+    }
+
+    private static ArrayList<NoteBooks> loadNoteBooksFromFile() throws IOException {
+        ArrayList<NoteBooks> noteBooks = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(noteBooksFile));
+            for (String line : lines) {
+                String[] fragments = line.split(";");
+                if (fragments.length != 10) {
+                    System.out.println("Неправильный размер строки" + line);
+                    continue;
+                }
+                noteBooks.add(new NoteBooks(
+                        Integer.parseInt(fragments[0]),
+                        fragments[1],
+                        fragments[2],
+                        fragments[3],
+                        fragments[4],
+                        fragments[5],
+                        fragments[6],
+                        Double.parseDouble(fragments[7]),
+                        fragments[8],
+                        fragments[9]
+                ));
+            }
+
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return noteBooks;
+    }
+}
---
+++ src/HW_6/NoteBooks.java
@@ -0,0 +1,143 @@
+package HW_6;
+
+import java.util.Objects;
+
+public class NoteBooks {
+    int id;
+    String brand;
+    String model;
+    String screenResolution;
+    String processor;
+    String ram;
+    String hardDrive;
+    double price;
+    String operatingSystem;
+    String color;
+    public NoteBooks(){
+    }
+
+    public NoteBooks(int id, String brand, String model, String screenResolution, String processor,
+                     String ram, String hardDrive, double price, String operatingSystem, String color) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.screenResolution = screenResolution;
+        this.processor = processor;
+        this.ram = ram;
+        this.hardDrive = hardDrive;
+        this.price = price;
+        this.operatingSystem = operatingSystem;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getScreenResolution() {
+        return screenResolution;
+    }
+
+    public void setScreenResolution(String screenResolution) {
+        this.screenResolution = screenResolution;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    public String getHardDrive() {
+        return hardDrive;
+    }
+
+    public void setHardDrive(String hardDrive) {
+        this.hardDrive = hardDrive;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NoteBooks noteBooks = (NoteBooks) o;
+        return id == noteBooks.id && brand.equals(noteBooks.brand) && model.equals(noteBooks.model) && Objects.equals(screenResolution, noteBooks.screenResolution) && processor.equals(noteBooks.processor) && ram.equals(noteBooks.ram) && hardDrive.equals(noteBooks.hardDrive) && Objects.equals(color, noteBooks.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, brand, model, screenResolution, processor, ram, hardDrive, color);
+    }
+
+    @Override
+    public String toString() {
+        return "NoteBooks{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", screenResolution='" + screenResolution + '\'' +
+                ", processor='" + processor + '\'' +
+                ", ram='" + ram + '\'' +
+                ", hardDrive='" + hardDrive + '\'' +
+                ", price=" + price +
+                ", operatingSystem='" + operatingSystem + '\'' +
+                ", color='" + color + '\'' +
+                '}' + "\n";
+    }
+
+}
+
